6c5f0597d4f0636a3b0eb0bdaa7cf6b3daafcb55
Justin John <activejobckr@gmail.com>
2022-07-11 14:13:35 +0530

    working model

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/loosec9n/Project_Three_Cache-Datastructures
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import "fmt"
+
+const SIZE = 5
+
+type Node struct {
+	Val   string
+	Left  *Node
+	Right *Node
+}
+
+type Queue struct {
+	Head   *Node
+	Tail   *Node
+	Length int
+}
+
+type Hash map[string]*Node
+
+type Cache struct {
+	Queue Queue
+	Hash  Hash
+}
+
+func NewCache() Cache {
+	return Cache{Queue: NewQueue(), Hash: Hash{}}
+}
+
+func NewQueue() Queue {
+	head := &Node{}
+	tail := &Node{}
+
+	head.Right = tail
+	tail.Left = head
+
+	return Queue{Head: head, Tail: tail}
+}
+
+func (c *Cache) Check(str string) {
+	node := &Node{}
+
+	if val, ok := c.Hash[str]; ok {
+		node = c.Remove(val)
+	} else {
+		node = &Node{Val: str}
+	}
+	c.Add(node)
+	c.Hash[str] = node
+}
+
+func (c *Cache) Remove(n *Node) *Node {
+	fmt.Printf("remove: %s\n", n.Val)
+	left := n.Left
+	right := n.Right
+
+	left.Right = right
+	right.Left = left
+	c.Queue.Length -= 1
+	delete(c.Hash, n.Val)
+	return n
+}
+
+func (c *Cache) Add(n *Node) {
+	fmt.Printf("add: %s\n", n.Val)
+	tmp := c.Queue.Head.Right
+
+	c.Queue.Head.Right = n
+	n.Left = c.Queue.Head
+	n.Right = tmp
+	tmp.Left = n
+
+	c.Queue.Length++
+	if c.Queue.Length > SIZE {
+		c.Remove(c.Queue.Tail.Left)
+	}
+}
+
+func (c *Cache) Display() {
+	c.Queue.Display()
+}
+
+func (q *Queue) Display() {
+	node := q.Head.Right
+	fmt.Printf("%d -[", q.Length)
+	for i := 0; i < q.Length; i++ {
+		fmt.Printf("{%s}", node.Val)
+		if i < q.Length-1 {
+			fmt.Printf("<-->")
+		}
+		node = node.Right
+	}
+	fmt.Println("]")
+}
+
+func main() {
+	fmt.Println("Start Cache")
+	cache := NewCache()
+
+	for _, word := range []string{
+		"Lion",
+		"Elephant",
+		"Monkey",
+		"Deer",
+		"Dragon",
+		"Alien",
+		"Monkey"} {
+		cache.Check(word)
+		cache.Display()
+	}
+}
